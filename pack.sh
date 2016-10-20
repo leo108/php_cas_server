494b9b6406bf50731c4250c64b189687f9e0be39
leo108 <leo108@qq.com>
2016-10-20 17:16:28 +0800

    pack script

--- pack.sh
+++ pack.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-rm -rf public/build public/css public/js
+rm -rf public/build public/css public/js build.zip
 yarn
 npm run prod
-git clean -e public -e vendor -dxf
-zip -r build.zip . -x ".git/*" -x build.zip
+git clean -e public -e vendor -e '.env' -e '.idea' -dxf
+zip -q -r build.zip . -x ".git/*" -x build.zip -x ".idea/*" -x ".env"
